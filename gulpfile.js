573417af93d0d1ac222430b17cfac8b480d3d205
Tobias Kiertscher <tobias.kiertscher@bosch.com>
2016-01-13 10:37:44 +0100

    completed build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 /* global require */
 var fs = require('fs');
+var del = require('del');
 var gulp = require('gulp');
 var textTransformation = require('gulp-text-simple');
+var rename = require('gulp-rename');
 var less = require('gulp-less');
 var cssnano = require('gulp-cssnano');
 
@@ -15,24 +17,32 @@ var fillBody = function (html, opts) {
 		});
 }
 
-gulp.task('copy')
+gulp.task('clear-dist', function () {
+	return del(['dist/*']);
+});
 
-gulp.task('prepare-tests', function () {
+gulp.task('copy-images', ['clear-dist'], function () {
+	return gulp.src('src/img/*')
+		.pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('prepare-tests', ['clear-dist'], function () {
 	return gulp.src('src/html/tests/test.*.html')
 		.pipe(textTransformation(fillBody)())
 		.pipe(gulp.dest('dist'));
 });
 
-gulp.task('compile-css', function () {
-	return gulp.src('src/css/*.less')
+gulp.task('compile-css', ['clear-dist'], function () {
+	return gulp.src(['src/css/*.less', '!src/css/vars.less', '!src/css/layout.less'])
 		.pipe(less())
-		.pipe(cssnano())
 		.pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('copy-images', function () {
-	return gulp.src('src/img/*')
-		.pipe(gulp.dest('dist/img'));
+gulp.task('compile-minified-css', ['compile-css'], function () {
+	return gulp.src(['dist/css/style.*.css'])
+		.pipe(cssnano())
+		.pipe(rename({ extname: '.mini.css' }))
+		.pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('default', ['compile-css', 'copy-images', 'prepare-tests']);
+gulp.task('default', ['copy-images', 'prepare-tests', 'compile-minified-css']);
